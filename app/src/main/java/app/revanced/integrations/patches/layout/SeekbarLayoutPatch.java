b73b936a90fa60647fc424ca09362ce8577fede5
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2024-11-28 01:24:35 +0900

    feat(custom-seekbar-color): use latest RVX code
    * Fix an issue that this patch is not working
    * Support light mode
    * Support changing color of thumbnail seekbar (watched videos)
    * Add an option to hide thumbnail seekbar

--- app/src/main/java/app/revanced/integrations/patches/layout/SeekbarLayoutPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/layout/SeekbarLayoutPatch.java
@@ -10,6 +10,8 @@ import app.revanced.integrations.utils.ReVancedUtils;
 
 public class SeekbarLayoutPatch {
 
+    public static final int ORIGINAL_SEEKBAR_COLOR = 0xFFFF0000;
+
     public static boolean enableSeekbarTapping() {
         return SettingsEnum.ENABLE_SEEKBAR_TAPPING.getBoolean();
     }
@@ -18,7 +20,23 @@ public class SeekbarLayoutPatch {
         return SettingsEnum.HIDE_TIME_AND_SEEKBAR.getBoolean();
     }
 
-    public static int enableCustomSeekbarColor(int colorValue) {
+    /**
+     * Overrides all drawable color that use the YouTube seekbar color.
+     * Used only for the video thumbnails seekbar.
+     * <p>
+     * If {@link Settings#HIDE_SEEKBAR_THUMBNAIL} is enabled, this returns a fully transparent color.
+     */
+    public static int getColor(int colorValue) {
+        if (colorValue == ORIGINAL_SEEKBAR_COLOR) {
+            if (SettingsEnum.HIDE_SEEKBAR_THUMBNAIL.getBoolean()) {
+                return 0x00000000;
+            }
+            return overrideSeekbarColor(ORIGINAL_SEEKBAR_COLOR);
+        }
+        return colorValue;
+    }
+
+    public static int overrideSeekbarColor(int colorValue) {
         if (SettingsEnum.ENABLE_CUSTOM_SEEKBAR_COLOR.getBoolean()) {
             try {
                 colorValue = Color.parseColor(SettingsEnum.ENABLE_CUSTOM_SEEKBAR_COLOR_VALUE.getString());
