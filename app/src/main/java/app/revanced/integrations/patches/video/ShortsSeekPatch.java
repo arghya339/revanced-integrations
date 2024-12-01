50a098b90aacdf4a77493cddf266a493767f3de2
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2024-12-01 16:55:38 +0900

    feat(shorts-seek): If camera button is appeared, replace camera button with skip button instead of menu button

--- app/src/main/java/app/revanced/integrations/patches/video/ShortsSeekPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/video/ShortsSeekPatch.java
@@ -12,6 +12,12 @@ import app.revanced.integrations.utils.ResourceType;
 public class ShortsSeekPatch {
 
     /**
+     * Whether replaced one of camera button or 3-dots menu button.
+     * Reset to false every time replaced the search button.
+     */
+    private static boolean replacedCameraOrMenuButton = false;
+
+    /**
      * @return whether hooked
      */
     public static boolean hookToolBar(Enum<?> buttonEnum, ImageView imageView) {
@@ -22,10 +28,10 @@ public class ShortsSeekPatch {
             return false;
         View view = (View) imageView.getParent();
 
-        final boolean useLongSeek = SettingsEnum.SHORTS_SEEK_AMOUNT.getBoolean();
-
         if (enumString.equals("SEARCH_FILLED")) {
+            replacedCameraOrMenuButton = false;
             // hook search button
+            final boolean useLongSeek = SettingsEnum.SHORTS_SEEK_AMOUNT.getBoolean();
             imageView.setImageResource(
                     identifier(useLongSeek ? "ic_seek_back_10_fill_36px" : "ic_seek_back_5_fill_36px", ResourceType.DRAWABLE)
             );
@@ -33,12 +39,16 @@ public class ShortsSeekPatch {
             return true;
         }
 
-        if (enumString.equals("MORE_VERT")) {
+        if (enumString.equals("SHORTS_HEADER_CAMERA") || enumString.equals("MORE_VERT")) {
+            if (replacedCameraOrMenuButton)
+                return false;
             // hook 3-dots menu button
+            final boolean useLongSeek = SettingsEnum.SHORTS_SEEK_AMOUNT.getBoolean();
             imageView.setImageResource(
                     identifier(useLongSeek ? "ic_seek_forward_10_fill_36px" : "ic_seek_forward_5_fill_36px", ResourceType.DRAWABLE)
             );
             view.setOnClickListener(v -> PlayerController.skipRelativeMilliseconds(useLongSeek ? 10000 : 5000));
+            replacedCameraOrMenuButton = true;
             return true;
         }
 
